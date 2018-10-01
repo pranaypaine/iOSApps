46093786702c15d3c7da43675db60c9ed4b9b354
Pranay Kumar Paine <pranaypaine@pranays-macbook-pro.local>
2018-10-02 01:21:46 +0530

    App Swoosh - UI design.

---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,26 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Pranay Paine on 02/10/18.
+//  Copyright © 2018 Pranay Paine. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 3.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+}
