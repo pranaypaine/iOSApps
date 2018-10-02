c0cddfdedb2572dd4548cd28c1abde523a0b2c44
Pranay Kumar Paine <pranaypaine@pranays-macbook-pro.local>
2018-10-02 11:25:22 +0530

    Auto layout -- fix for iPhone x series later.

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    
+    @IBOutlet weak var bgimg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        // Programatic layout acn be used for setting the layout but it is not the right solution
+        // swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        //
+        // bgimg.frame = view.frame
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
